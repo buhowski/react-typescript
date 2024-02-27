521b699f327eb6786a1215d87eca31cc8d53887a
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-02-27 22:16:33 +0200

    fix ext

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -208,7 +208,7 @@ module.exports = function (webpackEnv) {
 			chunkFilename: isEnvProduction
 				? 'static/js/[name].[contenthash:8].chunk.js'
 				: isEnvDevelopment && 'static/js/[name].chunk.js',
-			assetModuleFilename: 'static/media/[name].[ext]',
+			assetModuleFilename: 'static/media/[name][ext]',
 			// webpack uses `publicPath` to determine where the app is being served from.
 			// It requires a trailing slash, or the file assets will get an incorrect path.
 			// We inferred the "public path" (such as / or /my-project) from homepage.
