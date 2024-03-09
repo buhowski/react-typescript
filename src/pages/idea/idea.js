bb043ad6fcd03e54fa2c473027fb94fba05b8c84
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-03-10 01:16:26 +0200

    add refactore example

---
+++ src/pages/idea/idea.js
@@ -0,0 +1,69 @@
+import './Idea.scss';
+import IdeaTabs from './IdeaTabs';
+import textData from './textData';
+
+const Idea = () => {
+	const contactBtnTitle = 'Get In Touch';
+
+	// Function to render a block of content with title, texts, and lists
+	const renderBlock = (
+		title: string,
+		texts: string[],
+		listTitle: string,
+		lists: string[][]
+	) => (
+		<div className='idea-block'>
+			<h2 className='idea-block__title h2'>{title}</h2>
+
+			{texts.map((text, i) => (
+				<p key={i} className='idea-block__text'>
+					{text}
+				</p>
+			))}
+
+			{listTitle && <h3 className='h3'>{listTitle}</h3>}
+
+			{lists.map((items, i) => (
+				<ul key={i}>
+					{items.map((item, j) => (
+						<li key={j} className='idea-block'>
+							{item}
+						</li>
+					))}
+				</ul>
+			))}
+		</div>
+	);
+
+	// Function to render each idea item
+	const renderIdea = (data: any, index: number) => (
+		<div key={index}>
+			<h1 className='startup-title'>Startup Presentation</h1>
+
+			{renderBlock(
+				data.missionTitle,
+
+				[data.missionText],
+				'',
+				[data.missionList]
+			)}
+
+			<p className='last-words'>{data.lastWords}</p>
+		</div>
+	);
+
+	// Rendering idea items based on data
+	const renderedItems = textData.map(renderIdea);
+
+	return (
+		// Rendering idea tabs with localized content and contact button title
+		<IdeaTabs
+			IdeaTabRu={renderedItems[0]}
+			IdeaTabEn={renderedItems[1]}
+			IdeaTabUa={renderedItems[2]}
+			contactBtnTitle={contactBtnTitle}
+		/>
+	);
+};
+
+export default Idea;
