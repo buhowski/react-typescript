3eddd4fa2f970801b7c193fc5848e25fe081d5ec
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-03-20 10:44:09 +0200

    workers off

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
 // import { onSuccess, onUpdate } from './regStatus';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
+	// TODO: learn tests
 	// <React.StrictMode>
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>
 	// </React.StrictMode>
 );
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
@@ -23,9 +24,9 @@ root.render(
 // 	onUpdate,
 // 	onSuccess,
 // });
-serviceWorkerRegistration.unregister();
+// serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
