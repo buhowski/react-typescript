8f9a32c43c137833940b095d14dea79d202cb0fb
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-04-07 22:35:45 +0300

    дфіе цщкві

--- src/pages/startups/data/lastWords.tsx
+++ src/pages/startups/data/lastWords.tsx
@@ -1,11 +1,11 @@
-export const lastWordsEn = `For cooperation, partnership or team creation,
+export const lastWordsEn = `For cooperation, partnership or brainstorm together, 
 please contact me!
-Thank you for your attention.`;
+Thanks for attention.`;
 
-export const lastWordsUa = `Для співпраці, партнерства чи створення команди,
-будь ласка зв'яжіться зі мною!
+export const lastWordsUa = `Для співпраці, партнерства чи спільного брейнштурму, 
+будь ласка, зв'яжіться зі мною!
 Дякую за увагу.`;
 
-export const lastWordsRu = `Для сотрудничества, партнерства или создания команды,
+export const lastWordsRu = `Для сотрудничества, партнерства или совместного брейншторма,
 пожалуйста, свяжитесь со мной!
 Спасибо за внимание.`;
