6cffab98dc506ad5159092237595af89543c5816
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-04-15 06:12:38 +0300

    add new last words

--- src/pages/startups/data/lastWords.tsx
+++ src/pages/startups/data/lastWords.tsx
@@ -1,11 +1,5 @@
-export const lastWordsEn = `For cooperation, partnership or brainstorm together, 
-please contact me!
-Thanks for attention.`;
+export const lastWordsEn = `Like-minded people and everyone interested in collaboration - please contact me for partnership or brainstorm together!`;
 
-export const lastWordsUa = `Для співпраці, партнерства чи спільного брейнштурму, 
-будь ласка, зв'яжіться зі мною!
-Дякую за увагу.`;
+export const lastWordsUa = `Однодумці та всі, бажаючі співпрацювати - будь ласка, зв'яжіться зі мною, для партнерства чи сумісного брейншторму!`;
 
-export const lastWordsRu = `Для сотрудничества, партнерства или совместного брейншторма,
-пожалуйста, свяжитесь со мной!
-Спасибо за внимание.`;
+export const lastWordsRu = `Единомышленники и все, желающие сотрудничать - пожалуйста, свяжитесь со мной, для партнерства или совместного брейншторма!`;
