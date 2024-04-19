045d62a505e0d84b41a274d1953b61a5ec1b691c
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-04-19 12:57:18 +0300

    last words fix

--- src/pages/startups/data/lastWords.tsx
+++ src/pages/startups/data/lastWords.tsx
@@ -1,5 +1,11 @@
-export const lastWordsEn = `For like-minded people and everyone interested in collaboration - please contact me!`;
+export const lastWordsEn = `For like-minded people and everyone 
+interested in collaboration -
+please contact me!`;
 
-export const lastWordsUa = `Для однодумців та всіх бажаючих співпрацювати - будь ласка, зв'яжіться зі мною!`;
+export const lastWordsUa = `Для однодумців та всіх бажаючих 
+співпрацювати - будь ласка,
+зв'яжіться зі мною!`;
 
-export const lastWordsRu = `Для единомышленников и всех желающих сотрудничать - пожалуйста, свяжитесь со мной!`;
+export const lastWordsRu = `Для единомышленников и всех желающих 
+сотрудничать - пожалуйста, 
+свяжитесь со мной!`;
