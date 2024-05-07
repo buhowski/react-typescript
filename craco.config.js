f64172d0cc311f671bd83f137663af6e7884833b
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-05-07 23:45:02 +0300

    edit output .pdf file path using craco

--- craco.config.js
+++ craco.config.js
@@ -2,35 +2,23 @@
 module.exports = {
 	webpack: {
 		configure: (webpackConfig, { env, paths }) => {
+			// Modify the output path for PDFs specifically
 			webpackConfig.module.rules.push({
-				// Remove [contenthash] after file names
-				test: /\.(pdf|jpg|png|gif|webp|mp4)$/,
+				test: /\.pdf$/, // Target only PDF files
+				type: 'asset/resource',
+				generator: {
+					filename: '[name][ext]', // Set your desired folder name
+				},
+			});
+
+			// Remove [contenthash] after file names
+			webpackConfig.module.rules.push({
+				test: /\.(jpg|png|gif|webp|mp4)$/,
 				type: 'asset/resource',
 				generator: {
 					filename: 'static/media/[name][ext]',
 				},
 			});
-
-			// webpackConfig.resolve.fallback = {
-			// 	http: false,
-			// 	http2: false,
-			// 	https: false,
-			// 	buffer: false,
-			// 	url: false,
-			// 	querystring: false,
-			// 	os: false,
-			// 	stream: false,
-			// 	path: false,
-			// 	util: false,
-			// 	crypto: false,
-			// 	zlib: false,
-			// 	process: false,
-			// 	assert: false,
-			// 	fs: false,
-			// 	child_process: false,
-			// 	net: false,
-			// 	tls: false,
-			// };
 
 			// Other CRA Webpack
 			return webpackConfig;
