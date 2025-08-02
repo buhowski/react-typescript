97bc461853e0c70cc702e8cc02ea4bf324d2859f
buhowski <olexander.tsiomakh@gmail.com>
2025-08-03 00:25:51 +0300

    test

--- craco.config.js
+++ craco.config.js
@@ -3,13 +3,13 @@ module.exports = {
 	webpack: {
 		configure: (webpackConfig, { env, paths }) => {
 			// Modify the output path for PDFs specifically
-			webpackConfig.module.rules.push({
-				test: /\.pdf$/, // Target only PDF files
-				type: 'asset/resource',
-				generator: {
-					filename: '[name][ext]', // Set your desired folder name
-				},
-			});
+			// webpackConfig.module.rules.push({
+			// 	test: /\.pdf$/, // Target only PDF files
+			// 	type: 'asset/resource',
+			// 	generator: {
+			// 		filename: '[name][ext]', // Set your desired folder name
+			// 	},
+			// });
 
 			// Remove [contenthash] after file names
 			// webpackConfig.module.rules.push({
