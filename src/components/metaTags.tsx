c76c76e5163b9e844942d687a46c68b4b6d50868
Olexander Tsiomakh <olexander.tsiomakh@gmail.com>
2025-08-23 13:50:34 +0300

    testing new seo

--- src/components/metaTags.tsx
+++ src/components/metaTags.tsx
@@ -1,62 +1,78 @@
+import { pathToStartup, pathToAbout, pathToProjects } from './urlsData';
+
+// Base Website URL
+const website = 'https://buhowski.dev';
+const buildUrl = (path: string) => `${website}${path}`;
+
+// Open Graph Images
+// const ogImageHome = `${website}/screenshot_home.jpg`;
+const ogImageStartup = `${website}/screenshot_plan.jpg`;
+const ogImageAbout = `${website}/screenshot_about.jpg`;
+const ogImagePortfolio = `${website}/screenshot_portfolio.jpg`;
+
 // Main Website Meta Tags
-const website = 'https://buhowski.dev/';
+const author = 'Olexander Tsiomakh';
+const shortTitleAbout = 'Explorer | Developer | Creator';
+const shortTitlePortfolio = 'Portfolio | Projects Overview';
+const startupTitle = 'Another Dimension: Magazine | Media | Films | Games | Technologies';
 
-const shortAbout = 'Explorer | Developer | Creator';
+const defaultDescription =
+	'Building awesome products and contributing useful solutions to the world. Based on Planet Earth, Solar System.';
+const startupDescription =
+	'Creating an informational and entertaining magazine and transforming it into a company that produces its own technologies.';
+const aboutDescription =
+	'A developer and creator who builds innovative solutions at the intersection of tech and design.';
+const portfolioDescription =
+	'Explore portfolio, showcasing impactful projects developed for companies and innovative personal ventures.';
 
-const startupTitle = `Another Dimension: Magazine | Media | Films | Games | Technologies`;
-
-const defaultDescription = `Building awesome products and contributing useful solutions to the world. Based on Planet Earth, Solar System.`;
-
-const startupDescription = `Creating an informational and entertaining magazine and transforming it into a company that produces its own technologies.`;
-
-const aboutDescription = `A developer and creator who builds innovative solutions at the intersection of tech and design.`;
-
-const portfolioDescription = `Explore portfolio, showcasing impactful projects developed for companies and innovative personal ventures.`;
-
+// ### Default Meta Tags / Home Page
 export const defaultMetaTags = {
-	title: `Olexander Tsiomakh: ${shortAbout}`,
+	title: `${author}: ${shortTitleAbout}`,
 	description: defaultDescription,
 	canonicalUrl: website,
 
 	// Open Graph Meta Tags
 	ogUrl: website,
-	ogTitle: shortAbout,
+	ogTitle: shortTitleAbout,
 	ogDescription: defaultDescription,
-	ogImage: `${website}screenshot.png`,
+	ogImage: ogImageStartup,
 };
 
+// ### Business Plan Page
 export const startupsMetaTags = {
 	title: startupTitle,
 	description: startupDescription,
-	canonicalUrl: `${website}startup`,
+	canonicalUrl: buildUrl(pathToStartup),
 
 	// Open Graph Meta Tags
-	ogUrl: `${website}startup`,
+	ogUrl: buildUrl(pathToStartup),
 	ogTitle: startupTitle,
 	ogDescription: startupDescription,
-	ogImage: `${website}preview_startups.jpg`,
+	ogImage: ogImageStartup,
 };
 
+// ### About Page
 export const aboutMetaTags = {
-	title: `Olexander Tsiomakh: ${shortAbout}`,
+	title: `${author}: ${shortTitleAbout}`,
 	description: aboutDescription,
-	canonicalUrl: `${website}about`,
+	canonicalUrl: buildUrl(pathToAbout),
 
 	// Open Graph Meta Tags
-	ogUrl: `${website}about`,
-	ogTitle: shortAbout,
+	ogUrl: buildUrl(pathToAbout),
+	ogTitle: shortTitleAbout,
 	ogDescription: aboutDescription,
-	ogImage: `${website}preview_about.jpg`,
+	ogImage: ogImageAbout,
 };
 
+// ### Portfolio Page
 export const portfolioMetaTags = {
-	title: `Olexander Tsiomakh: Portfolio | Projects Overview`,
+	title: `${author}: ${shortTitlePortfolio}`,
 	description: portfolioDescription,
-	canonicalUrl: `${website}projects`,
+	canonicalUrl: buildUrl(pathToProjects),
 
 	// Open Graph Meta Tags
-	ogUrl: `${website}projects`,
-	ogTitle: `Portfolio | Projects Overview`,
+	ogUrl: buildUrl(pathToProjects),
+	ogTitle: shortTitlePortfolio,
 	ogDescription: portfolioDescription,
-	ogImage: `${website}preview_portfolio.jpg`,
+	ogImage: ogImagePortfolio,
 };
--- src/config/PageHelmet.tsx
+++ src/config/PageHelmet.tsx
@@ -5,10 +5,17 @@ interface MetaTags {
 	description?: string;
 	canonicalUrl?: string;
 
+	// Open Graph Meta Tags
 	ogUrl?: string;
 	ogTitle?: string;
 	ogDescription?: string;
 	ogImage?: string;
+
+	// Twitter Meta Tags
+	twitterUrl?: string;
+	twitterTitle?: string;
+	twitterDescription?: string;
+	twitterImage?: string;
 }
 
 interface PageHelmetProps {
@@ -16,12 +23,15 @@ interface PageHelmetProps {
 }
 
 const PageHelmet: React.FC<PageHelmetProps> = ({ metaTags }) => {
+	const finalTwitterUrl = metaTags.twitterUrl || metaTags.ogUrl;
+	const finalTwitterTitle = metaTags.twitterTitle || metaTags.ogTitle;
+	const finalTwitterDescription = metaTags.twitterDescription || metaTags.ogDescription;
+	const finalTwitterImage = metaTags.twitterImage || metaTags.ogImage;
+
 	return (
 		<Helmet>
 			{metaTags.title && <title>{metaTags.title}</title>}
-			{metaTags.description && (
-				<meta name='description' content={metaTags.description} />
-			)}
+			{metaTags.description && <meta name='description' content={metaTags.description} />}
 			{metaTags.canonicalUrl && <link rel='canonical' href={metaTags.canonicalUrl} />}
 
 			{/* Open Graph Meta Tags */}
@@ -33,12 +43,12 @@ const PageHelmet: React.FC<PageHelmetProps> = ({ metaTags }) => {
 			{metaTags.ogImage && <meta property='og:image' content={metaTags.ogImage} />}
 
 			{/* Twitter Meta Tags */}
-			{metaTags.ogUrl && <meta property='twitter:url' content={metaTags.ogUrl} />}
-			{metaTags.ogTitle && <meta name='twitter:title' content={metaTags.ogTitle} />}
-			{metaTags.ogDescription && (
-				<meta name='twitter:description' content={metaTags.ogDescription} />
+			{finalTwitterUrl && <meta property='twitter:url' content={finalTwitterUrl} />}
+			{finalTwitterTitle && <meta name='twitter:title' content={finalTwitterTitle} />}
+			{finalTwitterDescription && (
+				<meta name='twitter:description' content={finalTwitterDescription} />
 			)}
-			{metaTags.ogImage && <meta name='twitter:image' content={metaTags.ogImage} />}
+			{finalTwitterImage && <meta name='twitter:image' content={finalTwitterImage} />}
 		</Helmet>
 	);
 };
