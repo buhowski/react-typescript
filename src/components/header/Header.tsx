67c57cec8295b2f120b61be597dc6a4b0c6685d8
Olexander Tsiomakh <olexander.tsiomakh@gmail.com>
2026-03-11 00:07:11 +0200

    revert: Revert "fix: update email icon and colors"

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -93,8 +93,8 @@ const Header = () => {
 									items={[
 										{ id: 'github' },
 										{ id: 'linkedin' },
-										{ id: 'instagram' },
 										{ id: 'telegram' },
+										{ id: 'instagram' },
 									]}
 								/>
 							</nav>
