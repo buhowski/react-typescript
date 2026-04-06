46c53209786ab670531c4798d03bcf1407b6d2b5
Olexander Tsiomakh <olexander.tsiomakh@gmail.com>
2026-04-06 18:47:28 +0300

    test 9

--- src/pages/CVPage.tsx
+++ src/pages/CVPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Preloader from '../components/Preloader';
 import PageHelmet from '../components/PageHelmet';
 import { cvMetaTags } from '../components/metaTagsBasic';
@@ -7,6 +7,12 @@ import { pathToProjects } from '../components/urlsData';
 const docId = '12rOT1Pa4Z-Usau2Xkh-QTXweDTZJJTKvadrJKmRpCk0';
 const downloadDoc = `https://docs.google.com/document/d/${docId}/export?format=pdf`;
 const previewDoc = `https://docs.google.com/document/d/${docId}/preview?rm=minimal&chrome=false&embedded=true`;
+const previewPDF = `https://docs.google.com/viewer?url=${encodeURIComponent(downloadDoc)}&embedded=true`;
+const viewerDoc = `https://docs.google.com/viewer?srcid=${docId}&embedded=true`;
+
+// const fullPreviewDoc = `https://drive.google.com/file/d/${docId}/view`;
+
+const PREVIEW_URLS = [previewDoc, viewerDoc, previewPDF];
 
 const CVActions = () => {
 	return (
@@ -51,12 +57,45 @@ const CVActions = () => {
 				</svg>
 				Get CV
 			</a>
+
+			{/* <a
+				href={fullPreviewDoc}
+				target='_blank'
+				rel='noopener noreferrer'
+				className='resume__btn'
+				title='Open PDF version in new tab'
+			>
+				Open CV
+			</a> */}
 		</nav>
 	);
 };
 
 const CVPage = () => {
 	const [loaded, setLoaded] = useState(false);
+	const [urlIndex, setUrlIndex] = useState(0);
+
+	const handleLoad = () => {
+		setLoaded(true);
+	};
+
+	// IFRAME ERROR FALLBACK
+	const handleError = () => {
+		if (urlIndex < PREVIEW_URLS.length - 1) {
+			setUrlIndex((i) => i + 1);
+			setLoaded(false);
+		}
+	};
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (!loaded && urlIndex === 0) {
+				setUrlIndex(1);
+			}
+		}, 8000);
+
+		return () => clearTimeout(timer);
+	}, [loaded, urlIndex]);
 
 	return (
 		<div className={`resume ${loaded ? 'is-loaded' : ''}`}>
@@ -67,12 +106,13 @@ const CVPage = () => {
 			{!loaded && <Preloader />}
 
 			<iframe
-				onLoad={() => setLoaded(true)}
-				src={previewDoc}
+				key={urlIndex}
+				onLoad={handleLoad}
+				onError={handleError}
+				src={PREVIEW_URLS[urlIndex]}
 				title='Resume Preview'
 				className='resume__frame'
 				loading='lazy'
-				allowFullScreen
 				referrerPolicy='no-referrer'
 			/>
 		</div>
