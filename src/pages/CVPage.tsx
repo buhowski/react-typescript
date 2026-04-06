55928469a0f9c2a7f2151cc535f1e8d42bba7580
Olexander Tsiomakh <olexander.tsiomakh@gmail.com>
2026-04-06 22:06:27 +0300

    trying sharing option for google docs

--- src/pages/CVPage.tsx
+++ src/pages/CVPage.tsx
@@ -6,10 +6,10 @@ import { pathToProjects } from '../components/urlsData';
 
 const docId = '12rOT1Pa4Z-Usau2Xkh-QTXweDTZJJTKvadrJKmRpCk0';
 const docExportPDF = `https://docs.google.com/document/d/${docId}/export?format=pdf`;
-const doc = `https://docs.google.com/document/d/${docId}/preview?rm=minimal`;
+// const doc = `https://docs.google.com/document/d/${docId}/preview?rm=minimal&embedded=true`;
 
 //TODO: Make separate for chrome mobile condition
-// const doc = `https://docs.google.com/document/d/${docId}/edit?usp=sharing&rm=minimal`;
+const doc = `https://docs.google.com/document/d/${docId}/edit?usp=sharing&rm=minimal`;
 // previewDoc = `https://docs.google.com/viewer?url=${encodeURIComponent(downloadDoc)}&embedded=true`;
 
 const CVActions = ({ link, downloadFile }: { link: string; downloadFile: string }) => {
